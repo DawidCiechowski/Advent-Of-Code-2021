f3ab991fa3df290b9efe5469a5cdb05af11a67d4
Dawid Ciechowski <dawid.ciechowski@gmail.com>
2021-12-05 14:59:12 +0100

    :zap: day 4 finished

--- Day4/day4.py
+++ Day4/day4.py
@@ -1,4 +1,4 @@
-from typing import List, Any, Tuple
+from typing import List, Any, Tuple, Optional
 from pathlib import Path
 
 
@@ -7,7 +7,12 @@ def extract_boards(boards_str: List[str]) -> List[Any]:
     for i in range(0, len(boards_str), 6):
         board = []
         for j in range(1, 6):
-            board.append([int(value) for value in boards_str[i + j].strip().split()])
+            board.append(
+                [
+                    {"value": int(value), "seen": False}
+                    for value in boards_str[i + j].strip().split()
+                ]
+            )
 
         boards.append(board)
 
@@ -18,40 +23,76 @@ def read_input(test: bool = False) -> Tuple[List[int], List[Any]]:
     if test:
         with open(f"{Path(__file__).parent}/test_input.txt", "r") as f:
             values = [int(value) for value in f.readline().strip().split(",")]
-            boards_str = f.readlines()
-            boards = extract_boards(boards_str)
+            boards = extract_boards(f.readlines())
 
             return values, boards
 
     with open(f"{Path(__file__).parent}/input.txt", "r") as f:
-        values = f.readline().split(",")
-        boards_str = f.readlines()
-        boards = extract_boards(boards_str)
+        values = [int(value) for value in f.readline().strip().split(",")]
+        boards = extract_boards(f.readlines())
 
         return values, boards
 
 
-def part1(test: bool = False) -> int:
-    answer = 0
+def calculate_total(board: List[List[Any]]) -> int:
+    total = 0
+    for row in board:
+        for position in row:
+            if not position["seen"]:
+                total += position["value"]
+
+    return total
+
+
+def check_board(board: List[List[Any]]) -> bool:
+    for col_index, row in enumerate(board):
+        if all(position["seen"] == True for position in row):
+            return True
+
+        if all(board[row_index][col_index]["seen"] == True for row_index in range(5)):
+            return True
+
+    return False
+
+
+def part1(test: bool = False) -> Optional[int]:
     values, boards = read_input(test)
-    seen_numbers = []
-    
-    # Could just append first four numbers to seen numbers and start from index four
-    # But whaterver, this looks a bit better
-    for index, value in enumerate(values):
-        if index < 5:
-            seen_numbers.append(value)
-            continue
-        
+
+    for value in values:
         for board in boards:
             for row in board:
-                if all(value in seen_numbers for value in row):
-                    break
-        
+                for position in row:
+                    if value == position["value"]:
+                        position["seen"] = True
+                if check_board(board):
+                    return calculate_total(board) * value
+
+    return None
 
 
-def part2(test: bool = False) -> Any:
-    answer = 0
+def part2(test: bool = False) -> Optional[int]:
     values, boards = read_input(test)
+    winning_boards = []
+    for value in values:
+        for board in boards:
 
+            if board in winning_boards:
+                continue
+
+            for row in board:
+                for position in row:
+                    if value == position["value"]:
+                        position["seen"] = True
+                        break
+
+                if check_board(board):
+                    winning_boards.append(board)
+                    if len(boards) == len(winning_boards):
+                        return calculate_total(winning_boards[len(winning_boards) - 1]) * value
+                    else:
+                        break
+                else:
+                    continue
+            
+    return None
 
--- README.md
+++ README.md
@@ -27,5 +27,5 @@ This repository contains my implementation (**sometimes unnecessarily complicate
 
 ### [Day 4 - Giant Squid](https://adventofcode.com/2021/day/4)
 
-- [ ] Part 1
-- [ ] Part 2
+- [x] Part 1
+- [x] Part 2
